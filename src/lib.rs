d81642dd718f61c1d8cd2ef5e83d18ca516fca2a
Nakaryo <nakaryo716.dev@gmail.com>
2026-04-26 22:16:03 +0900

    move tests and fix ci

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,13 @@
 //! The [`channel`] function is used to create a [`Sender`] and [`Receiver`]
 //! handle pair that form the channel.
 //!
-//! The [`Sender`] handle is used by the producer to send the value.  
-//! The [`Receiver`] handle is used by the consumer to receive the value.
+//! - The [`Sender`] handle is used by the producer to send the value.
+//! - The [`Receiver`] handle is used by the consumer to receive the value.
 //!
 //! Each handle can be used on other threads.
 //!
-//! [`Sender::send`] will no block the calling thread.  
-//! [`Receiver::recv`] will **block** the calling thread.
+//! - [`Sender::send`] will no block the calling thread.
+//! - [`Receiver::recv`] will **block** the calling thread.
 //!
 //! # Example
 //! ```rust
@@ -78,7 +78,7 @@ pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
 
 /// Sends a value to the associated [`Receiver`].
 ///
-/// This is created by the [`channel`] function.  
+/// This is created by the [`channel`] function.
 /// Messages can be sent using [`send`](Sender::send).
 #[derive(Debug)]
 pub struct Sender<T> {
@@ -87,7 +87,7 @@ pub struct Sender<T> {
 
 /// Receive a value from the associated [`Sender`].
 ///
-/// This is created by the [`channel`] function.  
+/// This is created by the [`channel`] function.
 /// Messages sent to the channel can be retrieved using [`recv`](Receiver::recv).
 /// [`recv`](Receiver::recv) method blocks thread.
 #[derive(Debug)]
@@ -169,8 +169,9 @@ impl<T> Sender<T> {
         Ok(())
     }
 
-    /// Returns true if the associated Receiver handle has been dropped.  
-    /// A Receiver is closed by either calling close explicitly or the Receiver value is dropped.  
+    /// Returns true if the associated Receiver handle has been dropped.
+    ///
+    /// A Receiver is closed by either calling close explicitly or the Receiver value is dropped.
     /// If true is returned, a call to send will always result in an error.
     pub fn is_closed(&self) -> bool {
         let inner = self.inner.as_ref().unwrap();
@@ -462,298 +463,5 @@ impl fmt::Debug for State {
             .field("is_closed", &self.is_closed())
             .field("is_waiting", &self.is_waiting())
             .finish()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-
-    #[cfg(loom)]
-    use loom::thread;
-
-    #[cfg(not(loom))]
-    use std::thread;
-
-    use crate::{channel, error::TryRecvError};
-
-    #[test]
-    fn test_local() {
-        let test_inner = || {
-            let (tx, rx) = channel();
-
-            tx.send(5).unwrap();
-
-            let result = rx.recv().unwrap();
-            assert_eq!(result, 5);
-        };
-
-        #[cfg(loom)]
-        loom::model(test_inner);
-
-        #[cfg(not(loom))]
-        test_inner();
-    }
-
-    #[test]
-    fn test_thread_tx() {
-        let test_inner = || {
-            let (tx, rx) = channel();
-
-            thread::spawn(move || {
-                thread::yield_now();
-                tx.send(5).unwrap();
-            });
-
-            let result = rx.recv().unwrap();
-            assert_eq!(result, 5);
-        };
-
-        #[cfg(loom)]
-        loom::model(test_inner);
-
-        #[cfg(not(loom))]
-        test_inner();
-    }
-
-    #[test]
-    fn test_thread_rx() {
-        let test_inner = || {
-            let (tx, rx) = channel();
-
-            let result = thread::spawn(move || rx.recv().unwrap());
-
-            thread::yield_now();
-
-            tx.send(5).unwrap();
-            assert_eq!(result.join().unwrap(), 5);
-        };
-
-        #[cfg(loom)]
-        loom::model(test_inner);
-
-        #[cfg(not(loom))]
-        test_inner();
-    }
-
-    #[test]
-    fn test_rx_already_closed() {
-        let test_inner = || {
-            let (tx, rx) = channel();
-
-            drop(rx);
-
-            let result = tx.send(5);
-            assert!(result.is_err());
-        };
-
-        #[cfg(loom)]
-        loom::model(test_inner);
-
-        #[cfg(not(loom))]
-        test_inner();
-    }
-
-    #[test]
-    fn test_tx_already_closed() {
-        let test_inner = || {
-            let (tx, rx) = channel::<i32>();
-            drop(tx);
-
-            assert!(rx.recv().is_err());
-        };
-
-        #[cfg(loom)]
-        loom::model(test_inner);
-
-        #[cfg(not(loom))]
-        test_inner();
-    }
-
-    #[test]
-    fn test_tx_already_closed_wait() {
-        let test_inner = || {
-            let (tx, rx) = channel::<i32>();
-
-            thread::spawn(move || {
-                thread::yield_now();
-                drop(tx);
-            });
-
-            assert!(rx.recv().is_err());
-        };
-
-        #[cfg(loom)]
-        loom::model(test_inner);
-
-        #[cfg(not(loom))]
-        test_inner();
-    }
-
-    #[test]
-    fn test_rx_close() {
-        let test_inner = || {
-            let (tx, mut rx) = channel::<i32>();
-
-            rx.close();
-            assert!(tx.send(5).is_err());
-        };
-
-        #[cfg(loom)]
-        loom::model(test_inner);
-
-        #[cfg(not(loom))]
-        test_inner();
-    }
-
-    #[cfg(not(loom))]
-    #[test]
-    fn test_rx_close_thread() {
-        let (tx, mut rx) = channel();
-
-        std::thread::spawn(move || {
-            rx.close();
-        });
-
-        thread::sleep(std::time::Duration::from_millis(500));
-        assert!(tx.send(5).is_err());
-    }
-
-    #[test]
-    fn test_rx_recv_after_close() {
-        let test_inner = || {
-            let (_tx, mut rx) = channel::<i32>();
-
-            rx.close();
-            assert!(rx.recv().is_err());
-        };
-
-        #[cfg(loom)]
-        loom::model(test_inner);
-
-        #[cfg(not(loom))]
-        test_inner();
-    }
-
-    #[test]
-    fn test_rx_recv_after_send_and_close() {
-        let test_inner = || {
-            let (tx, mut rx) = channel();
-
-            tx.send(5).unwrap();
-            rx.close();
-
-            assert_eq!(5, rx.recv().unwrap());
-        };
-
-        #[cfg(loom)]
-        loom::model(test_inner);
-
-        #[cfg(not(loom))]
-        test_inner();
-    }
-
-    #[test]
-    fn test_is_closed() {
-        let test_inner = || {
-            let (tx, mut rx) = channel::<i32>();
-            rx.close();
-
-            assert!(tx.is_closed());
-        };
-
-        #[cfg(loom)]
-        loom::model(test_inner);
-
-        #[cfg(not(loom))]
-        test_inner();
-    }
-
-    #[test]
-    #[should_panic]
-    fn recv_after_try_recv() {
-        let test_inner = || {
-            let (tx, mut rx) = channel();
-
-            tx.send(5).unwrap();
-            assert_eq!(rx.try_recv().unwrap(), 5);
-            rx.recv().unwrap();
-        };
-
-        #[cfg(loom)]
-        loom::model(test_inner);
-
-        #[cfg(not(loom))]
-        test_inner();
-    }
-
-    #[test]
-    fn try_recv_after_close() {
-        let test_inner = || {
-            let (tx, mut rx) = channel::<i32>();
-
-            drop(tx);
-            assert_eq!(rx.try_recv(), Err(TryRecvError::Closed));
-        };
-
-        #[cfg(loom)]
-        loom::model(test_inner);
-
-        #[cfg(not(loom))]
-        test_inner();
-    }
-
-    #[test]
-    fn try_recv_empty() {
-        let test_inner = || {
-            let (_tx, mut rx) = channel::<i32>();
-
-            assert_eq!(rx.try_recv(), Err(TryRecvError::Empty));
-        };
-
-        #[cfg(loom)]
-        loom::model(test_inner);
-
-        #[cfg(not(loom))]
-        test_inner();
-    }
-
-    #[test]
-    fn send_close_recv() {
-        let test_inner = || {
-            let (tx, mut rx) = channel();
-            tx.send(5).unwrap();
-            rx.close();
-            assert_eq!(5, rx.try_recv().unwrap());
-
-            let (tx, mut rx) = channel();
-            tx.send(5).unwrap();
-            rx.close();
-            assert_eq!(5, rx.recv().unwrap());
-        };
-
-        #[cfg(loom)]
-        loom::model(test_inner);
-
-        #[cfg(not(loom))]
-        test_inner();
-    }
-
-    #[test]
-    fn test_race_send_drop() {
-        let test_inner = || {
-            let (tx, rx) = channel();
-
-            thread::spawn(move || {
-                let _ = tx.send(5);
-            });
-
-            drop(rx);
-        };
-
-        #[cfg(loom)]
-        loom::model(test_inner);
-
-        #[cfg(not(loom))]
-        test_inner();
     }
 }
